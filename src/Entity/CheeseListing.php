e45b184b7737bb661f2ea1251e71373db2113cff
Dorin <dorineltuta2006@yahoo.com>
2020-11-24 12:15:07 +0200

    Chapter9: The Serializer

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -4,14 +4,16 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
+use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ApiResource(
  *     collectionOperations={"get", "post"},
  *     itemOperations={
- *          "get"={"path"="ilikecheeses/{id}"},
- *          "put"},
+ *          "get"={},
+ *          "put"
+ *      },
  *     shortName="chesses"
  * )
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
@@ -52,7 +54,12 @@ class CheeseListing
      */
     private $isPublished;
 
-    public function getId(): ?int
+    public function __construct()
+    {
+    	$this->createdAt = new \DateTimeImmutable();
+    }
+
+	public function getId(): ?int
     {
         return $this->id;
     }
@@ -74,9 +81,9 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setTextDescription(string $description): self
     {
-        $this->description = $description;
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -98,11 +105,9 @@ class CheeseListing
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function getCreatedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return Carbon::instance($this->createdAt)->diffForHumans();
     }
 
     public function getIsPublished(): ?bool
