1300db90244f9e0a68d9f81a77e5ed016438c44e
Dorin <dorineltuta2006@yahoo.com>
2020-11-27 11:19:48 +0200

    Chapter11: Serialization Groups

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
@@ -14,111 +15,133 @@ use Doctrine\ORM\Mapping as ORM;
  *          "get"={},
  *          "put"
  *      },
+ *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
+ *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
  *     shortName="chesses"
  * )
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
  */
 class CheeseListing
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 * @Groups({"cheese_listing:read", "cheese_listing:write"})
+	 */
+	private $title;
 
-    /**
-     * @ORM\Column(type="text")
-     */
-    private $description;
+	/**
+	 * @ORM\Column(type="text")
+	 * @Groups({"cheese_listing:read"})
+	 */
+	private $description;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $price;
+	/**
+	 * @ORM\Column(type="integer")
+	 * @Groups({"cheese_listing:read", "cheese_listing:write"})
+	 */
+	private $price;
 
-    /**
-     * The price of this delicious cheese, in cents.
-     *
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+	/**
+	 * The price of this delicious cheese, in cents.
+	 *
+	 * @ORM\Column(type="datetime")
+	 */
+	private $createdAt;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isPublished;
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isPublished = false;
 
-    public function __construct()
-    {
-    	$this->createdAt = new \DateTimeImmutable();
-    }
+	public function __construct()
+	{
+		$this->createdAt = new \DateTimeImmutable();
+	}
 
 	public function getId(): ?int
-    {
-        return $this->id;
-    }
+	{
+		return $this->id;
+	}
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+	public function setTitle(string $title): self
+	{
+		$this->title = $title;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
+	public function getDescription(): ?string
+	{
+		return $this->description;
+	}
 
-    public function setTextDescription(string $description): self
-    {
-        $this->description = nl2br($description);
+	public function setDescription(string $description): self
+	{
+		$this->description = $description;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPrice(): ?int
-    {
-        return $this->price;
-    }
+	/**
+	 * The description of the cheese as raw text.
+	 *
+	 * @Groups({"cheese_listing:write"})
+	 */
+	public function setTextDescription(string $description): self
+	{
+		$this->description = nl2br($description);
 
-    public function setPrice(int $price): self
-    {
-        $this->price = $price;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function getPrice(): ?int
+	{
+		return $this->price;
+	}
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
+	public function setPrice(int $price): self
+	{
+		$this->price = $price;
 
-    public function getCreatedAtAgo(): string
-    {
-        return Carbon::instance($this->createdAt)->diffForHumans();
-    }
+		return $this;
+	}
 
-    public function getIsPublished(): ?bool
-    {
-        return $this->isPublished;
-    }
+	public function getCreatedAt(): ?\DateTimeInterface
+	{
+		return $this->createdAt;
+	}
 
-    public function setIsPublished(bool $isPublished): self
-    {
-        $this->isPublished = $isPublished;
+	/**
+	 * How long ago in text that this cheese listing was added.
+	 *
+	 * @Groups({"cheese_listing:read"})
+	 */
+	public function getCreatedAtAgo(): string
+	{
+		return Carbon::instance($this->createdAt)->diffForHumans();
+	}
 
-        return $this;
-    }
+	public function getIsPublished(): ?bool
+	{
+		return $this->isPublished;
+	}
+
+	public function setIsPublished(bool $isPublished): self
+	{
+		$this->isPublished = $isPublished;
+
+		return $this;
+	}
 }
