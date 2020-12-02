02ba340ca07c9e2fc736016597a540e7568e4d66
Dorin <dorineltuta2006@yahoo.com>
2020-12-02 14:20:53 +0200

    Chapter14: Pagination

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -23,7 +23,10 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *      },
  *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
  *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
- *     shortName="chesses"
+ *     shortName="chesses",
+ *     attributes={
+ *			"pagination_items_per_page"=5
+ *     }
  * )
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
  * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
